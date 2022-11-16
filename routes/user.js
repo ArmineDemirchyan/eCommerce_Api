d5813dc0d2d7f2825f5515864faf789c88458049
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-11-16 16:31:39 +0400

    get-users-stats done

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,7 @@
-const { verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 
@@ -43,21 +46,47 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
 
-    res.status(200).json({...others});
+    res.status(200).json({ ...others });
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-
 //GET ALL USERS
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new;
-    try {
-      const users = query ? await User.find().select("-password").sort({_id:-1}).limit(1) : await User.find().select("-password");
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().select("-password").sort({ _id: -1 }).limit(1)
+      : await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
